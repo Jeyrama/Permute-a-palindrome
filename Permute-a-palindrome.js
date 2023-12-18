93df472d3754f3b07b87b497cb38de6b49fc8756
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-18 12:17:34 -0800

    Update Permute-a-palindrome.js

--- Permute-a-palindrome.js
+++ Permute-a-palindrome.js
@@ -18,3 +18,12 @@ Hint:
 
 
 // Solution
+
+function permuteAPalindrome(input) {
+  return input
+    .split('')
+    .sort((a, b) => a.charCodeAt() - b.charCodeAt())
+    .join('')
+    .replace(/(.)\1/g, '')
+    .length <= 1;
+}
